013285a0aaecd32a9e36f555f6e996660e9cd631
Hellen Gideon <hellasticity@gmail.com>
2026-05-19 14:27:42 +0100

    Clean webview_flutter setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,111 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text(
-            "SMARTFLOW WORKING",
-            style: TextStyle(
-              fontSize: 28,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
+      home: WebViewScreen(),
+    );
+  }
+}
+
+class WebViewScreen extends StatefulWidget {
+  const WebViewScreen({super.key});
+
+  @override
+  State<WebViewScreen> createState() =>
+      _WebViewScreenState();
+}
+
+class _WebViewScreenState
+    extends State<WebViewScreen> {
+
+  late final WebViewController controller;
+
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = WebViewController()
+      ..setJavaScriptMode(
+        JavaScriptMode.unrestricted,
+      )
+      ..setNavigationDelegate(
+        NavigationDelegate(
+
+          onPageStarted: (url) {
+            setState(() {
+              isLoading = true;
+            });
+          },
+
+          onPageFinished: (url) {
+            setState(() {
+              isLoading = false;
+            });
+          },
+
         ),
+      )
+      ..loadRequest(
+        Uri.parse(
+          "https://hrm.felicitysolar.ng/login",
+        ),
+      );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: Stack(
+        children: [
+
+          WebViewWidget(
+            controller: controller,
+          ),
+
+          if (isLoading)
+            Container(
+              color: Colors.white,
+              child: Center(
+                child: Column(
+                  mainAxisAlignment:
+                  MainAxisAlignment.center,
+                  children: [
+
+                    Image.asset(
+                      'assets/icon.png',
+                      width: 120,
+                    ),
+
+                    SizedBox(height: 20),
+
+                    CircularProgressIndicator(),
+
+                    SizedBox(height: 15),
+
+                    Text(
+                      "Loading Smartflow...",
+                    ),
+
+                  ],
+                ),
+              ),
+            ),
+
+        ],
       ),
-    ),
-  );
+    );
+  }
 }
