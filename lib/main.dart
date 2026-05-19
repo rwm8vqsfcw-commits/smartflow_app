6387d893e10ae8fb8fd0da2ac3a35931466eb9a9
Hellen Gideon <hellasticity@gmail.com>
2026-05-19 14:13:33 +0100

    Remove webview test

--- lib/main.dart
+++ lib/main.dart
@@ -1,135 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(
+    const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const WebViewScreen(),
-    );
-  }
-}
-
-class WebViewScreen extends StatefulWidget {
-  const WebViewScreen({super.key});
-
-  @override
-  State<WebViewScreen> createState() =>
-      _WebViewScreenState();
-}
-
-class _WebViewScreenState
-    extends State<WebViewScreen> {
-
-  bool isLoading = true;
-
-  late final WebViewController controller;
-
-  @override
-  void initState() {
-    super.initState();
-
-    controller = WebViewController()
-      ..setJavaScriptMode(
-        JavaScriptMode.unrestricted,
-      )
-      ..setNavigationDelegate(
-        NavigationDelegate(
-
-          onPageStarted: (url) {
-            setState(() {
-              isLoading = true;
-            });
-          },
-
-          onPageFinished: (url) {
-            setState(() {
-              isLoading = false;
-            });
-          },
-
-          onNavigationRequest:
-              (NavigationRequest request) async {
-
-            final url = request.url;
-
-            if (
-            url.endsWith(".pdf") ||
-                url.endsWith(".doc") ||
-                url.endsWith(".docx") ||
-                url.contains("/storage/")
-            ) {
-
-              await launchUrl(
-                Uri.parse(url),
-                mode:
-                LaunchMode.externalApplication,
-              );
-
-              return NavigationDecision.prevent;
-            }
-
-            return NavigationDecision.navigate;
-          },
-        ),
-      )
-      ..loadRequest(
-        Uri.parse(
-          "https://hrm.felicitysolar.ng/login",
-        ),
-      );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      body: Stack(
-        children: [
-
-          WebViewWidget(
-            controller: controller,
-          ),
-
-          if (isLoading)
-            Container(
-              color: Colors.white,
-              child: Center(
-                child: Column(
-                  mainAxisAlignment:
-                  MainAxisAlignment.center,
-                  children: [
-
-                    Image.asset(
-                      'assets/icon.png',
-                      width: 120,
-                    ),
-
-                    SizedBox(height: 20),
-
-                    CircularProgressIndicator(),
-
-                    SizedBox(height: 15),
-
-                    Text(
-                      "Loading Smartflow...",
-                    ),
-
-                  ],
-                ),
-              ),
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            "SMARTFLOW WORKING",
+            style: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
             ),
-
-        ],
+          ),
+        ),
       ),
-    );
-  }
+    ),
+  );
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,7 +11,6 @@ import firebase_messaging
 import flutter_local_notifications
 import geolocator_apple
 import url_launcher_macos
-import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
@@ -20,5 +19,4 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
-  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
